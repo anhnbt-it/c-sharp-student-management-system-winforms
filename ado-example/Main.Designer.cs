7ab1e28336338e3daf0dc9be1474c02ecad591cc
Nguyễn Bá Tuấn Anh <anhnbt.it@gmail.com>
2021-04-16 21:09:53 +0700

    Initial commit

---
+++ ado-example/Main.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace ado_example
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.lblClassName = new System.Windows.Forms.Label();
+            this.lblAddNewClass = new System.Windows.Forms.Label();
+            this.cboClassName = new System.Windows.Forms.ComboBox();
+            this.txtAddNewClass = new System.Windows.Forms.TextBox();
+            this.btnAddClass = new System.Windows.Forms.Button();
+            this.btnDeleteClass = new System.Windows.Forms.Button();
+            this.lbStudents = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(427, 291);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(101, 23);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnAddProduct_Click);
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Location = new System.Drawing.Point(186, 27);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(168, 13);
+            this.lblWelcome.TabIndex = 1;
+            this.lblWelcome.Text = "CLASS MANAGEMENT SYSTEM";
+            // 
+            // lblClassName
+            // 
+            this.lblClassName.AutoSize = true;
+            this.lblClassName.Location = new System.Drawing.Point(36, 74);
+            this.lblClassName.Name = "lblClassName";
+            this.lblClassName.Size = new System.Drawing.Size(63, 13);
+            this.lblClassName.TabIndex = 2;
+            this.lblClassName.Text = "Class Name";
+            this.lblClassName.Click += new System.EventHandler(this.lblClassName_Click);
+            // 
+            // lblAddNewClass
+            // 
+            this.lblAddNewClass.AutoSize = true;
+            this.lblAddNewClass.Location = new System.Drawing.Point(20, 118);
+            this.lblAddNewClass.Name = "lblAddNewClass";
+            this.lblAddNewClass.Size = new System.Drawing.Size(79, 13);
+            this.lblAddNewClass.TabIndex = 3;
+            this.lblAddNewClass.Text = "Add New Class";
+            // 
+            // cboClassName
+            // 
+            this.cboClassName.FormattingEnabled = true;
+            this.cboClassName.Location = new System.Drawing.Point(120, 71);
+            this.cboClassName.Name = "cboClassName";
+            this.cboClassName.Size = new System.Drawing.Size(121, 21);
+            this.cboClassName.TabIndex = 4;
+            this.cboClassName.SelectedIndexChanged += new System.EventHandler(this.cboClassName_SelectedIndexChanged);
+            // 
+            // txtAddNewClass
+            // 
+            this.txtAddNewClass.Location = new System.Drawing.Point(120, 115);
+            this.txtAddNewClass.Name = "txtAddNewClass";
+            this.txtAddNewClass.Size = new System.Drawing.Size(121, 20);
+            this.txtAddNewClass.TabIndex = 5;
+            // 
+            // btnAddClass
+            // 
+            this.btnAddClass.Location = new System.Drawing.Point(85, 165);
+            this.btnAddClass.Name = "btnAddClass";
+            this.btnAddClass.Size = new System.Drawing.Size(75, 23);
+            this.btnAddClass.TabIndex = 6;
+            this.btnAddClass.Text = "Add Class";
+            this.btnAddClass.UseVisualStyleBackColor = true;
+            this.btnAddClass.Click += new System.EventHandler(this.btnAddClass_Click);
+            // 
+            // btnDeleteClass
+            // 
+            this.btnDeleteClass.Location = new System.Drawing.Point(166, 165);
+            this.btnDeleteClass.Name = "btnDeleteClass";
+            this.btnDeleteClass.Size = new System.Drawing.Size(75, 23);
+            this.btnDeleteClass.TabIndex = 7;
+            this.btnDeleteClass.Text = "Delete Class";
+            this.btnDeleteClass.UseVisualStyleBackColor = true;
+            this.btnDeleteClass.Click += new System.EventHandler(this.btnDeleteClass_Click);
+            // 
+            // lbStudents
+            // 
+            this.lbStudents.FormattingEnabled = true;
+            this.lbStudents.Location = new System.Drawing.Point(361, 71);
+            this.lbStudents.Name = "lbStudents";
+            this.lbStudents.Size = new System.Drawing.Size(167, 212);
+            this.lbStudents.TabIndex = 8;
+            this.lbStudents.SelectedIndexChanged += new System.EventHandler(this.lbStudents_SelectedIndexChanged);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 326);
+            this.Controls.Add(this.lbStudents);
+            this.Controls.Add(this.btnDeleteClass);
+            this.Controls.Add(this.btnAddClass);
+            this.Controls.Add(this.txtAddNewClass);
+            this.Controls.Add(this.cboClassName);
+            this.Controls.Add(this.lblAddNewClass);
+            this.Controls.Add(this.lblClassName);
+            this.Controls.Add(this.lblWelcome);
+            this.Controls.Add(this.btnExit);
+            this.Name = "frmMain";
+            this.Text = "Class Management System";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.Label lblClassName;
+        private System.Windows.Forms.Label lblAddNewClass;
+        private System.Windows.Forms.ComboBox cboClassName;
+        private System.Windows.Forms.TextBox txtAddNewClass;
+        private System.Windows.Forms.Button btnAddClass;
+        private System.Windows.Forms.Button btnDeleteClass;
+        private System.Windows.Forms.ListBox lbStudents;
+
+    }
+}
+
---
+++ ado-example/Main.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ado_example
+{
+    public partial class frmMain : Form
+    {
+        private string connectionString = "server=LAB35\\SQL2012;database=abc;uid=lab;pwd=";
+        private ErrorProvider classNameErrorProvider;
+        public frmMain()
+        {
+            InitializeComponent();
+
+            classNameErrorProvider = new ErrorProvider();
+            classNameErrorProvider.SetIconAlignment(txtAddNewClass, ErrorIconAlignment.MiddleRight);
+            classNameErrorProvider.SetIconPadding(txtAddNewClass, 2);
+            classNameErrorProvider.BlinkRate = 1000;
+            classNameErrorProvider.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.AlwaysBlink;
+        }
+
+        private void btnAddProduct_Click(object sender, EventArgs e)
+        {
+            //Form frmAddProduct = new NewProduct();
+            //frmAddProduct.Show();
+            var confirmResult = MessageBox.Show("Are you sure you want to exit?", "Confirm Exit", MessageBoxButtons.YesNo);
+            if (confirmResult == DialogResult.Yes)
+            {
+                this.Close();
+            }
+
+        }
+
+        private void dgvwProducts_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            this.loadClass();
+        }
+
+        private void loadClass()
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            string query = "SELECT * FROM Classes";
+            SqlDataAdapter dataAdapper = new SqlDataAdapter(query, conn);
+            DataTable dt = new DataTable();
+            dataAdapper.Fill(dt);
+
+            cboClassName.DataSource = dt;
+            cboClassName.DisplayMember = "ClassName";
+            cboClassName.ValueMember = "ClassID";
+            conn.Close();
+        }
+
+        private void lblClassName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddClass_Click(object sender, EventArgs e)
+        {
+            if (this.IsNameValid())
+            {
+                try
+                {
+                    SqlConnection conn = new SqlConnection(connectionString);
+                    if (conn.State == ConnectionState.Closed)
+                    {
+                        conn.Open();
+                    }
+                    string query = "INSERT INTO Classes (ClassName) VALUES ('" + txtAddNewClass.Text + "')";
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = conn;
+                    cmd.CommandText = query;
+                    cmd.CommandType = CommandType.Text;
+                    int rowsAffected = cmd.ExecuteNonQuery();
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Add Class Successfully.");
+                        this.loadClass();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Something went wrong!");
+                    }
+                    conn.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                classNameErrorProvider.SetError(txtAddNewClass, "Class Name is required.");
+            }
+        }
+
+        private bool IsNameValid()
+        {
+            // Determine whether the text box contains a zero-length string.
+            return (txtAddNewClass.Text.Length > 0);
+        }
+
+        private void btnDeleteClass_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(connectionString);
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+                string query = "DELETE FROM Classes WHERE ClassID = " + cboClassName.SelectedValue.ToString();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = query;
+                cmd.CommandType = CommandType.Text;
+                int rowsAffected = cmd.ExecuteNonQuery();
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Delete class successfully.");
+                    this.loadClass();
+                }
+                else
+                {
+                    MessageBox.Show("Something went wrong!");
+                }
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void cboClassName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                if (cboClassName.SelectedIndex != -1)
+                {
+
+                    SqlConnection conn = new SqlConnection(connectionString);
+                    if (conn.State == ConnectionState.Closed)
+                    {
+                        conn.Open();
+                    }
+                    string query = "SELECT * FROM Students WHERE ClassID = " + cboClassName.SelectedValue.ToString();
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(query, conn);
+                    DataSet ds = new DataSet();
+                    dataAdapter.Fill(ds, "tblStudents");
+                    lbStudents.DataSource = ds.Tables["tblStudents"];
+                    lbStudents.DisplayMember = "Name";
+                    lbStudents.ValueMember = "StudentID";
+                    conn.Close();
+                }
+
+
+            }
+            catch (Exception)
+            {
+
+
+            }
+        }
+
+        private void lbStudents_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (lbStudents.SelectedIndex > -1)
+                {
+                    SqlConnection conn = new SqlConnection(connectionString);
+                    if (conn.State == ConnectionState.Closed)
+                    {
+                        conn.Open();
+                    }
+                    string query = "SELECT * FROM Students WHERE StudentID = " + lbStudents.SelectedValue.ToString();
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = conn;
+                    cmd.CommandText = query;
+                    cmd.CommandType = CommandType.Text;
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    TextBox txtStudentName = new TextBox();
+                    txtStudentName.Text = dr.GetValue(0).ToString();
+                    MessageBox.Show(txtStudentName.Text);
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
